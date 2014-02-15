8c57f97eafc9a8c2250f6261aabd45068ca36e9f
Toshifumi Sakaguchi <siskrn@gmail.com>
2014-02-15 18:41:14 +0900

    不正引数用の例外クラスを追加

--- lib/Milter/SMTPAuth/Exception.pm
+++ lib/Milter/SMTPAuth/Exception.pm
@@ -2,18 +2,23 @@
 package Milter::SMTPAuth::Exception;
 
 use Exception::Class (
+	'Milter::SMTPAuth::ArgumentError' => {
+		fields => [ 'error_message' ],
+	},
 	'Milter::SMTPAuth::LoggerError' => {
 		fields => [ 'error_message' ],
 	},
 	);
 
-sub Milter::SMTPAuth::LoggerError::full_message {
-	my ( $this ) = @_;
+sub Milter::SMTPAuth::ArgumentError::full_message {
+    my ( $this ) = @_;
+    return $this->error_message;
+}
 
-	return $this->error_message;
+sub Milter::SMTPAuth::LoggerError::full_message {
+    my ( $this ) = @_;
+    return $this->error_message;
 }
 
 1;
-
-
 
