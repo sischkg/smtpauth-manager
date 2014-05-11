edbadcf2cb05bcf847b32503dfad433990313c65
Toshifumi Sakaguchi <siskrn@gmail.com>
2014-05-12 01:07:52 +0900

    CentOS 6用のインストールスクリプトにて、正しくgroupを設定できない点を修正 設定ファイルのディレクトリ名が間違っている点を修正

--- data/centos6/install_postfix.sh
+++ data/centos6/install_postfix.sh
@@ -13,14 +13,16 @@ then
     useradd -g $GROUP -d /noexistent -s /bin/false $USER
 fi
 
-if ! getent passwd postfix
+if getent passwd postfix
 then
     gpasswd -a postfix $GROUP
 fi
 
-mkdir -p /etc/smtpauth-filter/
-touch /etc/smtpauth-filter/reject_ids.txt
+mkdir -p /etc/smtpauth/
+touch /etc/smtpauth/reject_ids.txt
 mkdir -p /var/log/smtpauth
+chown $USER:$GROUP /var/log/smtpauth
+mkdir -p /var/lib/smtpauth/rrd
 chown $USER:$GROUP /var/log/smtpauth
 cp smtpauth-manager /etc/init.d
 chmod 744 /etc/init.d/smtpauth-manager
