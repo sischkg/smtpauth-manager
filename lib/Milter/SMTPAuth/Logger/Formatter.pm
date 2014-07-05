7b7b0abf9bbde4e815445737d47771de938c7667
Toshifumi Sakaguchi <siskrn@gmail.com>
2014-07-06 01:19:32 +0900

    Webページに不要なファイルを削除

--- lib/Milter/SMTPAuth/Logger/Formatter.pm
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 mode:cperl -*-
-
-package Milter::SMTPAuth::Logger::Formatter;
-
-use Moose::Role;
-requires 'output';
-
-1;
--- lib/Milter/SMTPAuth/Logger/Outputter.pm
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 mode:cperl -*-
-
-package Milter::SMTPAuth::Logger::Outputter;
-
-use Moose::Role;
-requires 'output';
-requires 'close';
-
-1;
